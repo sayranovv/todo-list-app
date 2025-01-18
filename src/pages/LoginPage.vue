dc385ab4dc57d99ab9ea054529ea70be0b11e8e8
Eldar Sayranov <sayranoveldar@gmail.com>
2025-01-18 12:25:42 +0500

    fixed shadow

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -7,7 +7,6 @@ import RegistrationForm from '@/components/RegistrationForm.vue'
 
 <template>
   <main
-    v-auto-animate
     class="max-w-md mx-auto px-4 flex flex-col rounded-3xl gap-6 max-[450px]:h-screen bg-white min-[451px]:border-2 min-[451px]:border-black min-[451px]:p-7 min-[451px]:drop-shadow-2xl justify-between min-[451px]:h-[700px] min-[451px]:mt-10 overflow-hidden"
   >
     <div class="flex flex-col gap-6 max-[450px]:mt-4">
@@ -15,15 +14,15 @@ import RegistrationForm from '@/components/RegistrationForm.vue'
         <h1 class="text-6xl font-black text-center">ToDo List</h1>
         <h2 class="text-center font-semibold mt-2">Welcome!</h2>
       </div>
-      <Tabs default-value="Log In" class="w-full drop-shadow-lg">
-        <TabsList class="w-full rounded-full">
+      <Tabs default-value="Log In" class="w-full mb-5">
+        <TabsList class="w-full rounded-full drop-shadow-lg">
           <TabsTrigger value="Log In" class="w-1/2 rounded-full"> Log In </TabsTrigger>
           <TabsTrigger value="Sign In" class="w-1/2 rounded-full"> Sign In </TabsTrigger>
         </TabsList>
-        <TabsContent value="Log In" class="mt-4" v-auto-animate>
+        <TabsContent value="Log In" class="mt-4 drop-shadow-lg" v-auto-animate>
           <loginForm />
         </TabsContent>
-        <TabsContent value="Sign In" class="mt-4" v-auto-animate>
+        <TabsContent value="Sign In" class="mt-4 drop-shadow-lg" v-auto-animate>
           <RegistrationForm />
         </TabsContent>
       </Tabs>
